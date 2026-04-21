7909155e6a55c54735e9a0b6defe2747501c617d
SeemaC0507 <chaudhari.seema@gmail.com>
2026-04-21 18:40:41 -0500

    Remove venv and pycache permanently from git tracking

---
+++ config/config.py
@@ -0,0 +1,3 @@
+BASE_URL = "https://fakestoreapi.com"
+TIMEOUT = 10
+MAX_RESPONSE_MS = 3000
---
+++ tests/api/test_api_first.py
@@ -0,0 +1,36 @@
+import requests
+
+# def test_get_product_returns_200():
+#     response = requests.get("https://fakestoreapi.com/products/1")
+#     assert response.status_code == 200
+#     product = response.json()
+#     assert product["id"] == 1
+#     assert isinstance(product["title"], str)
+
+# def test_get_invalid_product_returns_404():
+#     response = requests.get("https://fakestoreapi.com/products/99999")
+#     assert response.status_code != 500
+
+def test_get_product_returns_200(api):
+    session, base_url = api
+    response = session.get(f"{base_url}/products/1")
+    assert response.status_code == 200
+    product = response.json()
+    assert product["id"] == 1
+    assert isinstance(product["title"],str)
+
+    elapsed = response.elapsed.total_seconds() * 1000
+    assert elapsed < 3000, f"Response too slow: {elapsed:.0f}ms - limit is 3000ms"
+
+def test_get_invalid_product_returns_404(api):
+    session, base_url = api
+    response = session.get(f"{base_url}/products/99999")
+    assert response.status_code != 500    
+
+def test_get_all_products_returns_list(api):
+    session, base_url = api
+    response = session.get(f"{base_url}/products")
+    assert response.status_code == 200
+    products = response.json()
+    assert isinstance(products, list)
+    assert len(products) > 0
